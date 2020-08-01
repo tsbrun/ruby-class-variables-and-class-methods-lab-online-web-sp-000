6147ad1ddf60e09edfb913ec2753e11cafd00608
Anya Brun <tsbrun@icloud.com>
2020-08-01 18:16:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,6 +43,7 @@ class Song
   def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
+      binding.pry
       if artist_count[artist]
         artist_count[artist] += 1
       else
